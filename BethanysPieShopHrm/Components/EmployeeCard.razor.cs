529b131b676b1d59726808aa60e5cd05253a0a67
Jason Fedler <Jason.Fedler@gmail.com>
2025-04-11 07:44:48 -0400

    initial commit after module 4 into 5

---
+++ BethanysPieShopHrm/Components/EmployeeCard.razor.cs
@@ -0,0 +1,10 @@
+using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.Components;
+
+public partial class EmployeeCard
+{
+    [Parameter]
+    public Employee Employee { get; set; } = default!;
+}
