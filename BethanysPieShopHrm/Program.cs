9c5fb5c8cb60f10b3d8cfa8cd494b0bf05a5cbfa
Jason Fedler <Jason.Fedler@gmail.com>
2025-04-15 14:34:41 -0400

    adds EF and seed data

--- BethanysPieShopHrm/Program.cs
+++ BethanysPieShopHrm/Program.cs
@@ -1,10 +1,17 @@
 using BethanysPieShopHRM.Components;
+using BethanysPieShopHRM.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContextFactory<AppDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("AppConnectionString"));
+});
 
 var app = builder.Build();
 
